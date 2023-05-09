7c3b1dbc5181dac7e0d8b68eb80b1a70023573a0
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-09 14:54:38 -0300

    Finalmente funcionou!!!

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ function mostrarFormulario() {
         document.getElementById('formulario').style.display = 'none';
     }
 }
+
+function adicionarTarefa() {
+    let tarefa = document.getElementById('idtarefa').value;
+    let data = document.getElementById('iddata').value;
+    let horario = document.getElementById('idhorario').value;
+
+    if(tarefa == '' || data == '' || horario == '') {
+        alert('Preencha todos os campos por favor!')
+    } else {
+        ocultaparagrafoinicial();
+        let tasks = document.querySelector('div.tasks');
+        let novaTask = document.createElement('div');
+        novaTask.innerHTML = `<p style="text-transform: uppercase; color: green">${tarefa}</p>
+                             <p>DATA: ${data}</p>
+                             <p>HORÁRIO: ${horario}</p>`
+    
+        mostrarFormulario();
+        tasks.appendChild(novaTask);
+    }
+
+}
+
+
+function ocultaparagrafoinicial() {
+    window.document.getElementById('paragrafoinicial').style.display = 'none';
+}
+
